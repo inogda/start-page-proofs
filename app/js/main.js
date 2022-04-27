055fa033fb428b8225b298f502df8e40e4b52cb0
inogda <inogdasms@gmail.com>
2022-04-27 20:01:30 +0300

    shablon header home people request header

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,14 @@ $(function(){
 
   
 });
+
+$(document).ready(function() {
+    $('.custom-file-input').on('change', function() {
+        realVal = $(this).val();
+        lastIndex = realVal.lastIndexOf('\\') + 1;
+        if(lastIndex !== -1) {
+            realVal = realVal.substr(lastIndex);
+            $(this).prev('.mask').find('.fileInputText').val(realVal);
+        }
+    });
+});
